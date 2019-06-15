644301c0c75afd2f250f1fd2a770286ba67876c4
AlexeyQian <alexeyqian@gmail.com>
2019-06-15 23:20:37 +0800

    refactory database class, reduce the class size and functions

--- tests/weku_tests/new_clazz.hpp
+++ tests/weku_tests/new_clazz.hpp
@@ -2,6 +2,7 @@
 
 #include <cstdlib>
 #include <vector>
+#include "hardfork_processor.hpp"
 
 #define HARDFORK_19 19u
 #define HARDFORK_19_BLOCK_NUM 1u // apply hardfork 19 on block 1
@@ -13,24 +14,27 @@
 #define HARDFORK_22_BLOCK_NUM 400u
 #define REQUIRED_HARDFORK_VOTES 17u
 
-typedef std::vector<std::pair<uint32_t, uint32_t>> hardfork_votes_type;
+typedef std::vector<std::pair<uint32_t, uint32_t> > hardfork_votes_type;
 
 struct signed_block
 {
 };
 
+class x_database;
+void process_hardfork(x_database& db);
+
 class x_database
 {
 public:
     void init_genesis();
-    uint32_t head_block_num(); 
+    uint32_t head_block_num() const; 
     std::vector<signed_block>& ledger_blocks();
 
-    uint32_t last_hardfork();  
-    const hardfork_votes_type& next_hardfork_votes() const;
-    void process_hardforks_until_19(); 
-    bool has_enough_hardfork_votes(uint32_t hardfork, uint32_t block_num);
-    void apply_hardfork(uint32_t hardfork);
+    uint32_t last_hardfork() const;  
+    void last_hardfork(uint32_t hardfork) {_last_hardfork = hardfork;} // TODO: need to encapsulate it.
+    const hardfork_votes_type& next_hardfork_votes() const;    
+    bool has_enough_hardfork_votes(uint32_t hardfork, uint32_t block_num) const;    
+    //void apply_hardfork(uint32_t hardfork);
     
     bool apply_block(const signed_block&);
     signed_block generate_block_before_apply();
@@ -51,7 +55,8 @@ void x_database::init_genesis()
 {
 }
 
-uint32_t x_database::head_block_num(){
+uint32_t x_database::head_block_num() const
+{
     return _head_block_num;
 }
 
@@ -60,7 +65,7 @@ std::vector<signed_block>& x_database::ledger_blocks()
     return _ledger_blocks;
 }
 
-uint32_t x_database::last_hardfork()
+uint32_t x_database::last_hardfork() const
 {
     return _last_hardfork;
 }
@@ -70,13 +75,9 @@ const hardfork_votes_type& x_database::next_hardfork_votes() const
     return _next_hardfork_votes;
 }
 
-void x_database::process_hardforks_until_19(){
-    _last_hardfork = 19;
-}
-
-bool x_database::has_enough_hardfork_votes(uint32_t hardfork, uint32_t block_num)
+bool x_database::has_enough_hardfork_votes(uint32_t hardfork, uint32_t block_num) const
 {
-    auto search_target = std::make_pair(hardfork, block_num);
+    const auto& search_target = std::make_pair(hardfork, block_num);
     uint32_t votes = 0;
     for(const auto& item : next_hardfork_votes())
         if(item.first == search_target.first && item.second == search_target.second)
@@ -85,12 +86,12 @@ bool x_database::has_enough_hardfork_votes(uint32_t hardfork, uint32_t block_num
     return votes >= REQUIRED_HARDFORK_VOTES;
 }
 
-void x_database::apply_hardfork(uint32_t hardfork)
-{
-    if (hardfork == 0)
-        throw std::runtime_error("cannot apply hardfork 0");
-    _last_hardfork = hardfork;
-}
+// void x_database::apply_hardfork(uint32_t hardfork)
+// {
+//     if (hardfork == 0)
+//         throw std::runtime_error("cannot apply hardfork 0");
+//     _last_hardfork = hardfork;
+// }
 
 bool x_database::apply_block(const signed_block& b){
 
@@ -98,36 +99,7 @@ bool x_database::apply_block(const signed_block& b){
 
     _head_block_num++;
     
-    switch(_head_block_num)
-    {        
-        // hardfork 01 - 19 should be all processed after block #1, 
-        // since we need to compatible with existing data based on previous STEEM code.
-        
-        // at this code been implemented, hardfork21 already have been applied,
-        // so we know exactly when hardfork 20 and hardfork 21 should be applied on which block.
-        // that's why we can pinpoint it to happen on specific block num
-        // but hardfork 22 is not happend yet, so we allow it to be triggered at a future block.
-        case 1u: 
-            process_hardforks_until_19();
-            break;
-        case HARDFORK_20_BLOCK_NUM:
-            // do_hardfork_20();
-            _last_hardfork = HARDFORK_20;
-            break;
-        case HARDFORK_21_BLOCK_NUM:
-            // DO_AHRDFORK_21();
-            _last_hardfork = HARDFORK_21;
-            break;        
-    }
-
-    if(last_hardfork() == HARDFORK_21 && head_block_num() >= HARDFORK_22_BLOCK_NUM) // need to vote to trigger hardfork 22, and so to future hardforks
-    {            
-        if(has_enough_hardfork_votes(HARDFORK_22, HARDFORK_22_BLOCK_NUM)){
-            // DO_AHRDFORK_22();
-            _last_hardfork = HARDFORK_22;
-        }
-             
-    }      
+    process_hardfork(*this);
         
     return true;
 }
@@ -161,6 +133,55 @@ signed_block x_database::debug_generate_block_until(uint32_t block_num)
 void x_database::debug_set_next_hardfork_votes(hardfork_votes_type next_hardfork_votes)
 {
     _next_hardfork_votes = next_hardfork_votes;
+}
+
+void apply_hardfork_01(x_database& db){
+    // applying code...
+}
+
+void apply_hardfork_22(x_database& db){
+    // applying code...
+}
+
+void apply_hardforks_to_19(x_database& _db){
+    apply_hardfork_01(_db);
+    // hardfork_applier_02 to 19 ...
+    //_db.last_hardfork(19);
+}
+
+
+void process_hardfork(x_database& _db)
+{
+    switch(_db.head_block_num())
+    {        
+        // hardfork 01 - 19 should be all processed after block #1, 
+        // since we need to compatible with existing data based on previous STEEM code.
+        
+        // at this code been implemented, hardfork21 already have been applied,
+        // so we know exactly when hardfork 20 and hardfork 21 should be applied on which block.
+        // that's why we can pinpoint it to happen on specific block num
+        // but hardfork 22 is not happend yet, so we allow it to be triggered at a future block.
+        case 1u: 
+            apply_hardforks_to_19(_db);
+            _db.last_hardfork(HARDFORK_19);
+            break;
+        case HARDFORK_20_BLOCK_NUM:
+            // do_hardfork_20();
+            _db.last_hardfork(HARDFORK_20);
+            break;
+        case HARDFORK_21_BLOCK_NUM:
+            // DO_AHRDFORK_21();
+            _db.last_hardfork(HARDFORK_21);
+            break;        
+    }
+
+    if(_db.last_hardfork() == HARDFORK_21 && _db.head_block_num() >= HARDFORK_22_BLOCK_NUM) // need to vote to trigger hardfork 22, and so to future hardforks
+    {            
+        if(_db.has_enough_hardfork_votes(HARDFORK_22, HARDFORK_22_BLOCK_NUM)){
+            apply_hardfork_22(_db);
+            _db.last_hardfork(HARDFORK_22);
+        }             
+    }   
 }
 
 struct x_database_fixture{
